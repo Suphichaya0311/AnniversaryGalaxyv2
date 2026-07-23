75f4aaa326bf8be48239c6dd5afd2917b9913f1f
Suphichaya0311 <oil.0877611417@gmail.com>
2026-07-23 13:15:20 +0700

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -1,1 +1,20 @@
+const startBtn = document.getElementById("startBtn");
+const intro = document.querySelector(".intro");
 
+startBtn.onclick = () => {
+
+intro.style.transition="1.2s";
+
+intro.style.opacity="0";
+
+intro.style.transform="translate(-50%,-50%) scale(.75)";
+
+document.body.classList.add("zoom");
+
+setTimeout(()=>{
+
+location.href="galaxy.html";
+
+},3000);
+
+}
